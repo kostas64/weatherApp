c3eeb28d4e021fd053856ec0fff6abd40e9e8619
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-01 20:51:11 +0200

    remove redundant api call

--- App.jsx
+++ App.jsx
@@ -1,16 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {getForecast} from './src/api';
 import Router from './src/navigation/Router';
 import ContextProvider from './src/context/Context';
 import StatusBarManager from './src/components/StatusBarManager';
 
 const App = () => {
-  useEffect(() => {
-    getForecast();
-  }, []);
-
   return (
     <NavigationContainer>
       <StatusBarManager>
