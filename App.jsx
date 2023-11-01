c1e3a4d0ebf5a73b32ff8150c263e6847ecd63d1
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-01 16:01:42 +0200

    api on app init

--- App.jsx
+++ App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
+import {getForecast} from './src/api';
 import Router from './src/navigation/Router';
 import ContextProvider from './src/context/Context';
 import StatusBarManager from './src/components/StatusBarManager';
 
 const App = () => {
+  useEffect(() => {
+    getForecast();
+  }, []);
+
   return (
     <NavigationContainer>
       <StatusBarManager>
