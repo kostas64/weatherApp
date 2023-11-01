03d8910854b5af566b5d4518228fff9844201e95
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-01 20:53:21 +0200

    fade animation on seven days

--- src/navigation/Router.jsx
+++ src/navigation/Router.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from '../screens/Home';
+import SevenDays from '../screens/SevenDays';
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
+  const fade = {
+    animation: 'fade',
+  };
+
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
+    <>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="SevenDays"
+          component={SevenDays}
+          options={{...fade}}
+        />
+      </Stack.Navigator>
+    </>
   );
 };
 
