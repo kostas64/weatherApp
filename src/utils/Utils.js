fc0dba107ac2723eef607ff39f62b0e33f829b3c
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-03 13:56:02 +0200

    add images to weathercode for background

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,3 +1,4 @@
+import {images} from '../assets/images';
 import {lottie} from '../assets/lottie';
 
 export const calcPrecipitation = forecastData => {
@@ -12,13 +13,21 @@ export const calcPrecipitation = forecastData => {
 
 export const getWeatherIconFromCode = code => {
   if (code === 0) {
-    return {icon: lottie.sunny, description: 'Sunny'};
+    return {icon: lottie.sunny, description: 'Sunny', img: images.sunny};
   } else if (code === 1) {
-    return {icon: lottie.dayCloud, description: 'Mostly Sunny'};
+    return {
+      icon: lottie.dayCloud,
+      description: 'Mostly Sunny',
+      img: images.dayCloud,
+    };
   } else if (code === 2 || code === 3) {
-    return {icon: lottie.dayManyClouds, description: 'Cloudy'};
+    return {
+      icon: lottie.dayManyClouds,
+      description: 'Cloudy',
+      img: images.dayManyClouds,
+    };
   } else if (code === 45 || code === 48) {
-    return {icon: lottie.foggy, description: 'Foggy'};
+    return {icon: lottie.foggy, description: 'Foggy', img: images.foggy};
   } else if (
     code === 56 ||
     code === 57 ||
@@ -29,7 +38,7 @@ export const getWeatherIconFromCode = code => {
     code === 85 ||
     code === 86
   ) {
-    return {icon: lottie.snowy, description: 'Snowy'};
+    return {icon: lottie.snowy, description: 'Snowy', img: images.snowy};
   } else if (
     code === 61 ||
     code === 63 ||
@@ -40,8 +49,8 @@ export const getWeatherIconFromCode = code => {
     code === 81 ||
     code === 82
   ) {
-    return {icon: lottie.rainy, description: 'Rainy'};
+    return {icon: lottie.rainy, description: 'Rainy', img: images.rainy};
   } else if (code === 95 || code === 96 || code === 99) {
-    return {icon: lottie.thunder, description: 'Storm'};
+    return {icon: lottie.thunder, description: 'Storm', img: images.thunder};
   }
 };
