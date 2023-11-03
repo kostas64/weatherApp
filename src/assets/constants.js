2a9a71292654b2264dc254405e219b267e7fcd8e
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-03 18:57:21 +0200

    constant isios + google places lib

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,7 @@
+import {Platform} from 'react-native';
 import {Dimensions} from 'react-native';
 
 export const WIDTH = Dimensions.get('window').width;
 export const HEIGHT = Dimensions.get('window').height;
+
+export const isIOS = Platform.OS === 'ios';
