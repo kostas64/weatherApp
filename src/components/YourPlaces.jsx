9b0c51b03d2a22b3954063a2043b727c94d61065
Konstantinos Efkarpidis <kostas11062@gmail.com>
2023-11-11 20:25:57 +0200

    haptik lib

--- src/components/YourPlaces.jsx
+++ src/components/YourPlaces.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {runOnJS} from 'react-native-reanimated';
 import {StyleSheet, View, FlatList} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
@@ -7,14 +8,36 @@ import {Context} from '../context/Context';
 import YourPlacesItem from './YourPlacesItem';
 import YourPlacesListHeader from './YourPlacesListHeader';
 
-const YourPlaces = ({animated, onPressAdd}) => {
-  const {yourPlaces} = React.useContext(Context);
-
-  const Separator = () => <View style={styles.separator} />;
+const YourPlaces = ({
+  animated,
+  onPressAdd,
+  closeButton,
+  longPressActivated,
+  setLongPressActivated,
+}) => {
+  const {yourPlaces, setLoadingApi, setYourPlaces} = React.useContext(Context);
 
   const renderItem = ({item, index}) => (
-    <YourPlacesItem item={item} index={index} />
+    <YourPlacesItem
+      key={index}
+      item={item}
+      index={index}
+      closeButton={closeButton}
+      onDismissItem={onDismissItem}
+      longPressActivated={longPressActivated}
+      setLongPressActivated={setLongPressActivated}
+    />
   );
+
+  const onDismissItem = item => {
+    'worklet';
+    runOnJS(setLoadingApi)({api1: true, api2: false});
+
+    const filteredPlaces = yourPlaces.filter(place => place.city !== item.city);
+    runOnJS(setYourPlaces)(filteredPlaces);
+
+    runOnJS(setLoadingApi)({api1: false, api2: false});
+  };
 
   const Wrapper = animated ? AnimatedFade : View;
 
@@ -27,8 +50,9 @@ const YourPlaces = ({animated, onPressAdd}) => {
       <FlatList
         horizontal
         data={yourPlaces}
+        scrollEnabled={true}
+        style={{height: 116}}
         renderItem={renderItem}
-        ItemSeparatorComponent={Separator}
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.listContainer}
       />
@@ -40,9 +64,6 @@ const styles = StyleSheet.create({
   listContainer: {
     paddingHorizontal: wp(6),
     paddingBottom: wp(8),
-  },
-  separator: {
-    width: wp(4),
   },
 });
 
--- src/components/YourPlacesItem.jsx
+++ src/components/YourPlacesItem.jsx
@@ -1,24 +1,109 @@
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  InteractionManager,
-} from 'react-native';
+import Animated, {
+  runOnJS,
+  withTiming,
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
 import React from 'react';
 import LottieView from 'lottie-react-native';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import {View, StyleSheet, InteractionManager, Text} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 import CText from './Text';
 import {colors} from '../assets/colors';
+import RemoveButton from './RemoveButton';
 import {Context} from '../context/Context';
 
-const YourPlacesItem = ({item, index}) => {
+const AnimText = Animated.createAnimatedComponent(Text);
+const AnimLottie = Animated.createAnimatedComponent(LottieView);
+
+const YourPlacesItem = ({
+  item,
+  closeButton,
+  onDismissItem,
+  longPressActivated,
+  setLongPressActivated,
+  index,
+}) => {
   const itemRef = React.useRef();
-  const {setSelectedPlace} = React.useContext(Context);
+  const itemOpacity = useSharedValue(1);
+  const itemSeparator = useSharedValue(16);
+  const cityWidth = useSharedValue(112);
+  const lottieWidth = useSharedValue(64);
+  const containerWidth = useSharedValue(278);
+  const paddingLeft = useSharedValue(8);
+  const paddingRight = useSharedValue(24);
+  const spaceLeft = useSharedValue(12);
+  const spaceRight = useSharedValue(24);
+
+  const {selectedPlace, setSelectedPlace} = React.useContext(Context);
 
   const onItemPress = () => {
-    setSelectedPlace(item);
+    if (selectedPlace.city !== item.city) {
+      setSelectedPlace(item);
+    }
   };
+
+  const onPressRemove = () => {
+    paddingLeft.value = 0;
+    paddingRight.value = 0;
+    spaceLeft.value = 0;
+    spaceRight.value = 0;
+    itemSeparator.value = 0;
+    itemOpacity.value = withTiming(0, {duration: 200});
+    containerWidth.value = withTiming(0, {duration: 250});
+    lottieWidth.value = withTiming(0, {duration: 250});
+    cityWidth.value = withTiming(0, {duration: 250}, isFinished => {
+      if (isFinished) {
+        onDismissItem(item);
+      }
+    });
+  };
+
+  const itemAnimStyle = useAnimatedStyle(() => ({
+    opacity: itemOpacity.value,
+    width: containerWidth.value,
+    paddingLeft: paddingLeft.value,
+    paddingRight: paddingRight.value,
+  }));
+
+  const lottieAnimStyle = useAnimatedStyle(() => ({
+    width: lottieWidth.value,
+  }));
+
+  const cityAnimStyle = useAnimatedStyle(() => ({
+    width: cityWidth.value,
+  }));
+
+  const spaceAnimStyle = useAnimatedStyle(() => ({
+    marginLeft: spaceLeft.value,
+    marginRight: spaceRight.value,
+  }));
+
+  const itemSepAnimStyle = useAnimatedStyle(() => ({
+    width: itemSeparator.value,
+  }));
+
+  const tapGesture = Gesture.Tap()
+    .onBegin(() => {
+      itemOpacity.value = withTiming(0.25, {duration: 100});
+    })
+    .onStart(() => runOnJS(onItemPress)())
+    .onFinalize(() => {
+      itemOpacity.value = withTiming(1, {duration: 150});
+    });
+
+  const longPressGesture = Gesture.LongPress()
+    .minDuration(300)
+    .onStart(() => {
+      console.log('Run long');
+      runOnJS(setLongPressActivated)(true);
+    })
+    .onFinalize(() => {
+      runOnJS(setLongPressActivated)(false);
+    });
 
   React.useEffect(() => {
     const interaction = InteractionManager.runAfterInteractions(() => {
@@ -29,52 +114,65 @@ const YourPlacesItem = ({item, index}) => {
   }, []);
 
   return (
-    <TouchableOpacity
-      key={index}
-      onPress={onItemPress}
-      style={styles.container}>
-      {/* Weather animation */}
-      <LottieView ref={itemRef} source={item.lottie} style={styles.lottie} />
+    <>
+      <RemoveButton
+        onPress={onPressRemove}
+        closeButton={closeButton}
+        itemPressed={longPressActivated}
+      />
+      <GestureDetector gesture={Gesture.Race(tapGesture, longPressGesture)}>
+        <View style={styles.row}>
+          <Animated.View key={index} style={[styles.container, itemAnimStyle]}>
+            {/* Weather animation */}
+            <AnimLottie
+              ref={itemRef}
+              source={item.lottie}
+              style={[styles.lottie, lottieAnimStyle]}
+            />
 
-      {/*  City & Desc & Temperature */}
-      <View style={styles.spaceHorizontal}>
-        <CText bold numberOfLines={1} style={styles.city}>
-          {item.city}
-        </CText>
-        <CText size={3}>{item.desc}</CText>
-      </View>
-      <CText>
-        <CText bold size={6}>
-          {Math.floor(item.temperature)}
-        </CText>
-        <CText size={6}>°</CText>
-      </CText>
-    </TouchableOpacity>
+            {/*  City & Desc & Temperature */}
+            <Animated.View style={spaceAnimStyle}>
+              <AnimText numberOfLines={1} style={[cityAnimStyle, styles.city]}>
+                {item.city}
+              </AnimText>
+              <CText size={3}>{item.desc}</CText>
+            </Animated.View>
+
+            {/* Temperature */}
+            <CText>
+              <CText bold size={6}>
+                {Math.floor(item.temperature)}
+              </CText>
+              <CText size={6}>°</CText>
+            </CText>
+          </Animated.View>
+          <Animated.View style={itemSepAnimStyle} />
+        </View>
+      </GestureDetector>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
   container: {
     marginTop: wp(3),
-    paddingLeft: wp(2),
-    paddingRight: wp(6),
-    borderRadius: wp(5),
+    borderRadius: 20,
     flexDirection: 'row',
     alignItems: 'center',
     overflow: 'hidden',
     backgroundColor: colors.darkBg,
   },
-  spaceHorizontal: {
-    marginLeft: wp(3),
-    marginRight: wp(6),
-  },
   lottie: {
-    top: wp(1),
-    width: wp(16),
-    height: wp(18),
+    top: 4,
+    height: 72,
   },
   city: {
-    width: wp(28),
+    color: 'white',
+    fontSize: wp(4),
+    fontFamily: 'Gilroy-Bold',
   },
 });
 
